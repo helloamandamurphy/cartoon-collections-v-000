2020ef7b737f40aefbafa6014035463fde9f2483
Amanda Murphy <helloamandamurphy@gmail.com>
2018-10-04 21:44:59 +0000

    automatically backed up by learn

--- cartoon_collections.rb
+++ cartoon_collections.rb
@@ -8,12 +8,12 @@ def summon_captain_planet(planeteer_calls)
   planeteer_calls.map! {|calls| "#{calls.capitalize}!"}
 end
 
-def long_planeteer_calls# code an argument here
-  # Your code here
+def long_planeteer_calls(calls)
+  calls.all? do |
 end
 
-def find_the_cheese# code an argument here
-  # the array below is here to help
+def find_the_cheese(snacks)
   cheese_types = ["cheddar", "gouda", "camembert"]
+  snacks.include?(cheese_types)
 end
 
